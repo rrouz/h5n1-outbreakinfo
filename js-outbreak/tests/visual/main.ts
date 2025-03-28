ec024cd1e0b74330c921ecff86045aa5a8f557e6
rrouz <ryinrouz94@gmail.com>
2025-03-28 10:11:47 -0700

    Linting

--- js-outbreak/tests/visual/main.ts
+++ js-outbreak/tests/visual/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
-import TestBarChart from './components/TestBarChart.vue'
-import TestScatterChart from './components/TestScatterChart.vue'
 import TestAllCharts from './components/TestAllCharts.vue'
 
 createApp(TestAllCharts).mount('#app')
