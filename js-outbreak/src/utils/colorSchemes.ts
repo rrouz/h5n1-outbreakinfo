88a5cff56e3dfbd943a12b78b7042b9ab2ba6ab1
rrouz <ryinrouz94@gmail.com>
2025-03-28 10:10:22 -0700

    Added outbreakInfo color scheme

---
+++ js-outbreak/src/utils/colorSchemes.ts
@@ -0,0 +1,31 @@
+import { schemeYlGnBu, interpolateRdPu } from 'd3-scale-chromatic'
+
+export const choroColorScheme = schemeYlGnBu;
+export const heatmapColorScheme = interpolateRdPu;
+
+export const defaultColor = '#3498db';
+
+export const colorPalette = [
+    '#bab0ab', // grey (other)
+    '#4E79A7', // dk blue
+    '#aecBe8', // lt blue
+    '#f28e2b', // orange
+    '#FFBE7D', // lt. orange
+    '#59a14f', // green
+    '#8CD17D', // lt. green
+    '#e15759', // red
+    '#FF9D9A', // lt. red
+    '#499894', // teal
+    '#86BCB6', // lt. teal
+    '#B6992D', // dk yellow
+    '#F1CE63', // yellow
+    '#D37295', // dk pink
+    '#FABFD2', // lt. pink,
+    '#B07AA1', // dk purple
+    '#D4A6C8', // lt. purple
+    '#9D7660', // brown
+    '#D7B5A6', // lt. brown
+    '#bcbd22', // puce
+    '#79706E', // grey
+];
+
