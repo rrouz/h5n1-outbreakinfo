277fd1f9e7097693634505f8eed2ccfa1d9c27bf
rrouz <ryinrouz94@gmail.com>
2025-03-28 10:09:30 -0700

    Added outbreakInfo module declaration

--- js-outbreak/src/vue.d.ts
+++ js-outbreak/src/vue.d.ts
@@ -3,3 +3,12 @@ declare module '*.vue' {
     const component: DefineComponent<{}, {}, any>
     export default component
 }
+
+// Add a new module declaration for your library
+declare module 'outbreakInfo' {
+    import { DefineComponent } from 'vue'
+
+    export const BarChart: DefineComponent<{}, {}, any>
+    export const ScatterChart: DefineComponent<{}, {}, any>
+    export const TimeSeriesChart: DefineComponent<{}, {}, any>
+}
