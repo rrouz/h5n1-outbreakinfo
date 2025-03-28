cb270fc7238d56189fbeade4ff214baf8cb3fcce
rrouz <ryinrouz94@gmail.com>
2025-03-28 09:56:38 -0700

    Changed app format to singular page

--- outbreak-info-h5n1/src/main.js
+++ outbreak-info-h5n1/src/main.js
@@ -1,46 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import IsolationSourceView from './views/IsolationSourceView.vue'
-import HostView from './views/HostSourceView.vue'
-import DMSView from './views/DMSView.vue'
-import CollectionEndDateView from './views/CollectionEndDateView.vue'
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: IsolationSourceView
-    },
-    {
-      path: '/isolations',
-      name: 'isolations',
-      component: IsolationSourceView
-    },
-    {
-      path: '/hosts',
-      name: 'hosts',
-      component: HostView
-    },
-    {
-      path: '/DMS',
-      name: 'dms',
-      component: DMSView
-    },
-    {
-      path: '/collection-end-dates',
-      name: 'collection-end-dates',
-      component: CollectionEndDateView
-    }
-  ]
-})
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
-
-router.push('/')
+// Simply create and mount the app without router
+createApp(App).mount('#app')
