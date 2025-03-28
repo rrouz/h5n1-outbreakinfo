99816f2cf7c52252a0b1f8fa7d7c85e4f3d5344c
rrouz <ryinrouz94@gmail.com>
2025-03-28 10:09:00 -0700

    outbreak-info color palette

--- js-outbreak/src/index.ts
+++ js-outbreak/src/index.ts
@@ -3,12 +3,17 @@ import BarChart from './components/BarChart.vue'
 import ScatterChart from './components/ScatterChart.vue'
 import TimeSeriesChart from './components/TimeSeriesChart.vue'
 
+// Import utilities
+import { colorPalette } from './utils/colorSchemes'
+
 // Export components
 export {
     BarChart,
     ScatterChart,
     TimeSeriesChart
 }
+
+export { colorPalette as outbreakInfoColorPalette }
 
 // Export default object for Vue.use()
 export default {
