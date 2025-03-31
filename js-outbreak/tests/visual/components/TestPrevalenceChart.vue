b9656e5be6a1424771cde00d2c3ba41c22559b2e
rrouz <ryinrouz94@gmail.com>
2025-03-31 13:24:21 -0700

    Add visual test for PrevalenceChart component with sample data

---
+++ js-outbreak/tests/visual/components/TestPrevalenceChart.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="test-prevalence-chart">
+        <h1>Prevalence Chart Test</h1>
+        <PrevalenceChart :demixedData="testData" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import PrevalenceChart from '../../../src/components/PrevalenceChart.vue'
+
+// Sample test data
+const testData = [
+    {
+        lineages: ['lineage1', 'lineage2', 'lineage3'],
+        abundances: [0.6, 0.3, 0.1]
+    },
+    {
+        lineages: ['lineage1', 'lineage2', 'lineage4'],
+        abundances: [0.7, 0.2, 0.1]
+    },
+    {
+        lineages: ['lineage2', 'lineage3', 'lineage4'],
+        abundances: [0.5, 0.3, 0.2]
+    },
+    {
+        lineages: ['lineage1', 'lineage3', 'lineage4'],
+        abundances: [0.4, 0.4, 0.2]
+    }
+]
+</script>
+
+<style scoped>
+.test-prevalence-chart {
+    padding: 20px;
+}
+</style> 
